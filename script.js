003339145e593a658ed90e4bb576824873a8a2f6
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-06-01 17:33:11 -0300

    Arrumei os erros com o Lint

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-
-
-
 // Função com objetivo de criar Listas ordenadas com os Baldes de Tintas para o usuário Selecionar.
-function criadorDePaletas(){
-  let cPaleta = document.querySelector('#color-palette');
-  let cores = ["black", "pink", "red", "olive"]
+function criadorDePaletas() {
+  const cPaleta = document.querySelector('#color-palette');
+  const cores = ['black', 'pink', 'red', 'olive'];
 
-  for (index = 0; index <= 3; index += 1) {
-    let li = document.createElement('li');
-    let cor = index;
+  for (let index = 0; index <= 3; index += 1) {
+    const li = document.createElement('li');
 
-    for (let j = 0; j < cores.length; j++) {
-      li.className = "color";
+    for (let j = 0; j < cores.length; j += 1) {
+      li.className = 'color';
       li.style.backgroundColor = cores[index];
       cPaleta.appendChild(li);
     }
@@ -19,18 +15,15 @@ function criadorDePaletas(){
 }
 // ============================ // ================================================= //
 
-function criadordeQuadro(){
-  let board = document.querySelector('#pixel-board');
-  let quadro = 25;
+function criadordeQuadro() {
+  const board = document.querySelector('#pixel-board');
 
-  for (let index = 0; index < 25; index +=1) {
-    let divPixel = document.createElement('div');
-    divPixel.className = "pixel";
+  for (let index = 0; index < 25; index += 1) {
+    const divPixel = document.createElement('div');
+    divPixel.className = 'pixel';
     board.appendChild(divPixel);
   }
-
 }
-
 
 criadordeQuadro();
 criadorDePaletas();
