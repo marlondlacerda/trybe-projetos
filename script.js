5cf7dfba68c971832d0022412955c0d50639199b
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-06-01 21:59:51 -0300

    código refatorado e lint usado

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-// Função com objetivo de criar Listas ordenadas com os Baldes de Tintas para o usuário Selecionar.
-function criadorDePaletas() {
-  const cPaleta = document.querySelector('#color-palette');
+window.onload = () => {
   const cores = ['black', 'pink', 'red', 'olive'];
+  const cPaleta = document.querySelector('#color-palette');
 
+  criadorDePaletas(cPaleta, cores);
+  criadordeQuadro();
+  seletorDeCor();
+};
+
+// Função com objetivo de criar Listas ordenadas com os Baldes de Tintas para o usuário Selecionar.
+function criadorDePaletas(cPaleta, cores) {
   for (let index = 0; index <= 3; index += 1) {
     const li = document.createElement('li');
 
@@ -12,9 +18,11 @@ function criadorDePaletas() {
       cPaleta.appendChild(li);
     }
   }
+  document.querySelector('.color').className += ' selected';
 }
 // ============================ // ================================================= //
 
+// CRIA O QUADRO E OS DIVS QUE REPRESENTARÁ OS PIXELS
 function criadordeQuadro() {
   const board = document.querySelector('#pixel-board');
 
@@ -25,5 +33,19 @@ function criadordeQuadro() {
   }
 }
 
-criadordeQuadro();
-criadorDePaletas();
+//   FUNÇÃO PARA SELECIONAR A COR DO BALDE
+function seletorDeCor() {
+  const btnColor = document.querySelectorAll('.color');
+  const corSelect = 'color selected';
+
+  for (let index = 0; index < btnColor.length; index += 1) {
+    btnColor[index].addEventListener('click', () => {
+      btnColor[0].className = 'color';
+      btnColor[1].className = 'color';
+      btnColor[2].className = 'color';
+      btnColor[3].className = 'color';
+      btnColor[index].className = corSelect;
+    });
+  }
+}
+// ============================ // ================================================= //
